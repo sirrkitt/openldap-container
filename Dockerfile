ba98aec93c669c0e52a26c36d8a1c17e8881d470
Jacob Louis Lemus Peschel <sirrkitt@gmail.com>
2020-09-09 23:59:30 -0700

    Reworked everything. Simple for now.

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,21 @@
-FROM sirrkitt/openldap-build-argon2 AS argon2
+FROM sirrkitt/openldap-builder AS builder
 FROM alpine:3.12
 LABEL maintainer="Jacob Lemus Peschel <jacob@tlacuache.us>"
-ENV BASEDN="dc=example,dc=org"
-ENV ROOTDN="root"
-ENV ROOTPW="secret"
-ENV PWHASH="{ARGON2}"
-ENV ENCROOTPW="NULL"
 ENV SSL="NOPE"
-ENV INIT="NO"
-ENV MAXDBSIZE="1073741824"
 
 COPY entrypoint.sh /entrypoint.sh
+COPY --from=builder /home/builder/packages/usr/x86_64/openldap*.apk /root/openldap.apk
 
-RUN	echo "@edge http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories &&\
-	apk update --no-cache && \
-	apk add -U --no-cache openldap@edge openldap-back-mdb@edge openldap-overlay-all@edge openldap-passwd-pbkdf2@edge openldap-passwd-sha2@edge argon2 libsodium-dev gettext && \
+RUN	apk update --no-cache && \
+	apk add -U --no-cache libsasl libuuid libltdl libldap libsodium libcrypto1.1 libssl1.1 && \
+	apk add --allow-untrusted /root/openldap.apk && \
 	mkdir -p /config /data /ssl /run/openldap /socket && \
+	addgroup --system ldap && \
+	adduser --system --ingroup ldap --disabled-password --no-create-home ldap && \
 	chown -R ldap:ldap /config /data /run/openldap /socket && \
 	chown -R root:root /ssl && \
 	chmod -R 0700 /config /data /ssl /socket &&\
 	chmod a+x /entrypoint.sh
-
-COPY slapd.gen	/etc/openldap/slapd.gen
-COPY db.gen	/etc/openldap/db.gen
-
-COPY --from=argon2 /usr/libexec/openldap/pw-argon2.a /usr/lib/openldap/pw-argon2.a
-COPY --from=argon2 /usr/libexec/openldap/pw-argon2.la /usr/lib/openldap/pw-argon2.la
-COPY --from=argon2 /usr/libexec/openldap/pw-argon2.so.0.0.0 /usr/lib/openldap/pw-argon2.so.0.0.0
-
-RUN ln -s /usr/lib/openldap/pw-argon2.so.0.0.0 /usr/lib/openldap/pw-argon2.so.0 &&\
-		ln -s /usr/lib/openldap/pw-argon2.so.0 /usr/lib/openldap/pw-argon2.so
 
 EXPOSE 389
 EXPOSE 636
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,57 +10,10 @@ elif [ ! -w "/data" ]
 then
 	echo "Unable to read or write data directory!"
 	return 1
-
-#if config database doesn't exist then create
-elif [ "$INIT" == "YES" ]
-then
-	if [ ! -e /config/cn\=config ] && [ ! -e /config/cn\=config.ldif ] && [ ! -e /data/data.mdb ] && [ ! -e /data/data.lock ] && [ ! -e /config/slapd.conf ]
-	then
-		echo "Generating config database!"
-		#database dn
-		echo "Setting basedn to $BASEDN"
-		echo "Setting rootdn to cn=$ROOTDN,$BASEDN"
-		echo "Selected password hash is $PWHASH"
-		#check if pass hash set for root password
-		if [ "$PWHASH" == "{MD5}" ] || [ "$PWHASH" == "{SMD5}" ] || [ "$PWHASH" == "{CRYPT}" ] || [ "$PWHASH" == "{SSHA}" ] || [ "$PWHASH" == "{SHA}" ] || [ "$PWHASH" == "{CLEARTEXT}" ] || [ "$PWHASH" == "{PBKDF2}" ] || [ "$PWHASH" == "{PBKDF2-SHA1}" ] || [ "$PWHASH" == "{PBKDF2-SHA256}" ] || [ "$PWHASH" == "{PBKDF-SHA512}" ] || [ "$PWHASH" == "{SHA256}" ] || [ "$PWHASH" == "{SSHA256}" ] || [ "$PWHASH" == "{SHA384}" ] || [ "$PWHASH" == "{SSHA384}" ] || [ "$PWHASH" == "{SHA512}" ] || [ "$PWHASH" == "{SSHA512}" ] || [ "$PWHASH" == "{ARGON2}" ]
-		then
-			echo "Valid password hash selected"
-			ENCROOTPW="$(/usr/sbin/slappasswd -o module-load=pw-sha2 -o module-load=pw-argon2 -s "$ROOTPW" -h "$PWHASH" -n)"
-		else
-			echo "Invalid password hash selected, try again!"
-			return 1
-		fi
-		#Create config
-		echo "Generating config LDIF"
-		/bin/cat /etc/openldap/slapd.gen|/usr/bin/envsubst>/config/slapd.conf
-		#copy config and fix permissions
-		echo "Generating ldif for initial DB"
-		/bin/cat /etc/openldap/db.gen|/usr/bin/envsubst>/etc/openldap/db.ldif
-		echo "Applying ldif"
-		/usr/sbin/slapadd -f /config/slapd.conf -l /etc/openldap/db.ldif -n1
-		echo "Applying proper permissions to everything"
-		/bin/chown -R ldap:ldap /config /ssl /data
-		echo "Init complete!"
-		return 0
-	fi
-
-	if [ -e /config/cn\=config.ldif ] || [ -e /config/cn\=config ] || [ -e /config/slapd.conf ]
-	then
-		echo "Config already exists! Unable to perform init"
-	fi
-	if [ -e /data/data.mdb ] || [ -e /data/data.lock ]
-	then
-		echo "Database already exists unable to perform init!"
-	fi
-	#fail because we can't init
-	return 1
 fi
-#if SSL option is set then listen on LDAPS as well
+
 if [ ! "$SSL" == "NOPE" ]
 then
-	echo "Starting slapd and listening on SSL"
 	exec /usr/sbin/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldap:/// ldaps:/// ldapi://%2Fsocket%2Fldapi" -d 0
 else
-	echo "Starting slapd"
 	exec /usr/sbin/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldap:/// ldapi://%2Fsocket%2Fldapi" -d 0
-fi
