514c066bffe6bc5812e1cac3e594a3659efebe58
Jacob Louis Lemus Peschel <sirrkitt@gmail.com>
2020-10-30 21:52:21 -0700

    Fix more typos

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ then
 	return 1
 fi
 
-addgroup -S ldap -g $GID ldap
+addgroup -S -g $GID ldap
 adduser -S -H -h /data -G ldap -g ldap -u $UID -s /sbin/nologin ldap
 
 chown -R ldap:ldap /data /config /ssl /run/openldap /socket
@@ -22,55 +22,26 @@ if [ "$CONFIG" == "OLD" ]
 then
 	if [ "$NET" == "NOPE" ]
 	then
-		if [ "$DEBUG" == "NOPE" ]
-		then
-			exec /usr/lib/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldapi://%2Fsocket%2Fldapi" -d 0
-		else
-			exec /usr/lib/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldapi://%2Fsocket%2Fldapi" -d 255
-		fi
+		exec /usr/lib/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldapi://%2Fsocket%2Fldapi" -d $debug
 	else
-		if [ "$DEBUG" == "NOPE" ]
+		if [ "$SSL" == "NOPE" ]
 		then
-			if [ "$SSL" == "NOPE" ]
-			then
-				exec /usr/lib/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldap:/// ldapi://%2Fsocket%2Fldapi" -d 0
-			else
-				exec /usr/lib/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldap:/// ldaps:/// ldapi://%2Fsocket%2Fldapi" -d 0
-			fi
+			exec /usr/lib/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldap:/// ldapi://%2Fsocket%2Fldapi" -d $debug
 		else
-			if [ "$SSL" == "NOPE" ]
-			then
-				exec /usr/lib/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldap:/// ldapi://%2Fsocket%2Fldapi" -d 255
-			else
-				exec /usr/lib/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldap:/// ldaps:/// ldapi://%2Fsocket%2Fldapi" -d 255
-			fi
+			exec /usr/lib/slapd -u ldap -g ldap -f /config/slapd.conf -h "ldap:/// ldaps:/// ldapi://%2Fsocket%2Fldapi" -d $debug
 		fi
 	fi
 fi
 
 if [ "$NET" == "NOPE" ]
 then
-	if [ "$DEBUG" == "NOPE" ]
-	then
-		exec /usr/lib/slapd -u ldap -g ldap -F /config/slapd.d -h "ldapi://%2Fsocket%2Fldapi" -d 0
-	else
-		exec /usr/lib/slapd -u ldap -g ldap -F /config/slapd.d -h "ldapi://%2Fsocket%2Fldapi" -d 255
-	fi
+	exec /usr/lib/slapd -u ldap -g ldap -F /config/slapd.d -h "ldapi://%2Fsocket%2Fldapi" -d $debug
 else
-	if [ "$DEBUG" == "NOPE" ]
-	then
 		if [ "$SSL" == "NOPE" ]
 		then
-			exec /usr/lib/slapd -u ldap -g ldap -F /config/slapd.d -h "ldap:/// ldapi://%2Fsocket%2Fldapi" -d 0
+			exec /usr/lib/slapd -u ldap -g ldap -F /config/slapd.d -h "ldap:/// ldapi://%2Fsocket%2Fldapi" -d $debug
 		else
-			exec /usr/lib/slapd -u ldap -g ldap -F /config/slapd.d -h "ldap:/// ldaps:/// ldapi://%2Fsocket%2Fldapi" -d 0
-		fi
-	else
-		if [ "$SSL" == "NOPE" ]
-		then
-			exec /usr/lib/slapd -u ldap -g ldap -F /config/slapd.d -h "ldap:/// ldapi://%2Fsocket%2Fldapi" -d 255
-		else
-			exec /usr/lib/slapd -u ldap -g ldap -F /config/slapd.d -h "ldap:/// ldaps:/// ldapi://%2Fsocket%2Fldapi" -d 255
+			exec /usr/lib/slapd -u ldap -g ldap -F /config/slapd.d -h "ldap:/// ldaps:/// ldapi://%2Fsocket%2Fldapi" -d $debug
 		fi
 	fi
 fi
